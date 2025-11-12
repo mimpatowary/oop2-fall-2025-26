0aa784746d8cc0c80b845e29b996971ce6e739a5
Mac <mac@Mimus-MacBook-Pro.local>
2025-11-12 19:24:16 +0600

    23-51156-1

---
+++ Lab/HouseExecrise/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+public class House  
+
+{
+
+    public int YearBuilt { get; set; }
+
+    public double Size { get; set; }
+
+
+
+    public House(int yearBuilt, double size)
+
+    {
+
+        YearBuilt = yearBuilt;
+
+        Size = size;
+
+    }
+
+    
+
+    private int HowOld()
+
+    {
+
+        int currentYear = DateTime.Now.Year;
+
+        return currentYear - YearBuilt;
+
+    }
+
+
+
+    public bool CanBeSold()
+
+    {
+
+        int age = HowOld();
+
+        return age > 15;
+
+    }
+
+
+
+    public void ShowInfo()
+
+    {
+
+        Console.WriteLine();
+
+        Console.WriteLine("--- House Info ---");
+
+        Console.WriteLine($"Year Built: {YearBuilt}, Size: {Size} sqft");
+
+        Console.WriteLine($"Can be sold: {CanBeSold()}");
+
+        Console.WriteLine();
+
+    }
+
+}
+
+class Program
+
+{
+
+    public static void Main()
+
+    {
+
+        Console.Write("Enter the year built of your house: ");
+
+        int yearBuilt = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the size of your house (in sqft): ");
+
+        double size = Convert.ToDouble(Console.ReadLine());
+
+
+
+        House house = new House(yearBuilt, size);
+
+        
+
+        house.ShowInfo();
+
+    }
+
+}
+ 
