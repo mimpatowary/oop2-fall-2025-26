d93d8e6612b577171adf549296f15011021d5829
Mac <mac@Mimus-MacBook-Pro.local>
2025-12-15 15:32:32 +0600

    23-51156-1

---
+++ Lab/Interface/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+interface IPrintable
+
+{
+    void Print();
+}
+
+interface ISerializable
+
+{
+    void Save();
+}
+
+
+class Report : IPrintable, ISerializable
+
+{
+    public void Print()
+    {
+
+        Console.WriteLine("painting report...");
+
+    }
+    public void Save()
+
+    {
+
+        Console.WriteLine("Saving report...");
+
+    }
+
+}
+class Program
+
+{
+    static void Main()
+
+    {
+
+        Report report = new Report();
+
+        report.Print();
+
+        report.Save();
+
+    }
+
+}
+ 
---
+++ Lab/StudentRecordsystem/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+ 
+interface IStudent
+{
+    void Display();
+}
+ 
+class Student : IStudent
+{
+    private int[] marks = new int[3];
+    private string name;
+ 
+    
+    public Student(string name)
+    {
+        this.name = name;
+    }
+ 
+
+    public int this[int index]
+    {
+        get
+        {
+            return marks[index];
+        }
+        set
+        {
+            marks[index] = value;
+        }
+    }
+ 
+    
+    public void Display()
+    {
+        Console.WriteLine("Student Name: " + name);
+        Console.WriteLine("Marks:");
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.WriteLine("Subject " + (i + 1) + ": " + marks[i]);
+        }
+    }
+}
+ 
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student("Mim ");
+ 
+        s[0] = 85;
+        s[1] = 87;
+        s[2] = 96;
+ 
+        s.Display();
+    }
+}
+
+ 
